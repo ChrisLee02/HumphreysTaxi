f37361c451b2da297b109f54970a482f4418820c
ChrisLee02 <hdchris765@snu.ac.kr>
2024-05-16 15:11:03 +0900

    "make controller to handle status check"

---
+++ src/main/java/com/example/demo/controller/StatusController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+    @GetMapping("")
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("");
+    }
+}
